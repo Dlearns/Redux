2d464b88683aa248985f954799f1d47d1fd6f074
Dlearns <166824706+Dlearns@users.noreply.github.com>
2024-04-16 19:44:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import CounterComponent from "./CounterComponent";
 
 function App() {
-  const counterIds = Array.from(Array(5).keys()); // Create an array of 1000 counter IDs
+  const counterIds = Array.from(Array(5).keys()); // Create an array of 5 counter IDs
 
   return (
     <div className="App">
